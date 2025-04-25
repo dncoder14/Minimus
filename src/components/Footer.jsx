fdfac915f08d7ed5db1be828fcced78ce37bb4cb
dncoder14 <dhirajpandit5050@gmail.com>
2025-04-25 19:18:25 +0530

    removing a link from footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,6 @@ const Footer = () => {
       <p>© {new Date().getFullYear()} Minimus. All rights reserved.</p>
       <div className="footer-links">
         <a href="https://github.com/dncoder14" target="_blank" rel="noopener noreferrer">GitHub</a>
-        <a href="https://www.tvmaze.com/api" target="_blank" rel="noopener noreferrer">TVmaze API</a>
       </div>
     </footer>
   );
